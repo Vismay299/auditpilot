dd7c2506a03106e3cb8df3661b001c7cb4c66dff
Vismay Rathod <vismayrathod29@gmail.com>
2026-02-19 22:39:09 -0500

    Fix ESLint build errors: escape apostrophe in login page, lock Render to free plan

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -76,7 +76,7 @@ export default function LoginPage({
                 </CardContent>
                 <CardFooter>
                     <div className="text-sm text-center w-full">
-                        Don't have an account?{' '}
+                        Don&apos;t have an account?{' '}
                         <Link href="/signup" className="underline">
                             Sign up
                         </Link>
